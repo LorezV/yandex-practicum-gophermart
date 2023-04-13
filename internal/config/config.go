7470978babca456057dd4bdd9ebb77151bab077e
Дмитрий Деркач <dima.derkach2004@gmail.com>
2023-04-13 17:55:56 +0500

    fix

--- internal/config/config.go
+++ internal/config/config.go
@@ -9,10 +9,16 @@ import (
 const SecretKey = "EXAMPLE_SECRET_KEY"
 const PasswordSalt = "EXAMPLE_PASSWORD_SALT"
 
+//var Config struct {
+//	RunAddress           string `env:"RUN_ADDRESS" envDefault:"localhost:8000"`
+//	AccrualSystemAddress string `env:"ACCRUAL_SYSTEM_ADDRESS " envDefault:"http://127.0.0.1:8080"`
+//	DatabaseURI          string `env:"DATABASE_URI" envDefault:"postgres://postgres:admin@localhost:5432/go-diploma"`
+//}
+
 var Config struct {
-	RunAddress           string `env:"RUN_ADDRESS" envDefault:"localhost:8000"`
-	AccrualSystemAddress string `env:"ACCRUAL_SYSTEM_ADDRESS " envDefault:"http://127.0.0.1:8080"`
-	DatabaseURI          string `env:"DATABASE_URI" envDefault:"postgres://postgres:admin@localhost:5432/go-diploma"`
+	RunAddress           string `env:"RUN_ADDRESS"`
+	AccrualSystemAddress string `env:"ACCRUAL_SYSTEM_ADDRESS "`
+	DatabaseURI          string `env:"DATABASE_URI"`
 }
 
 func InitConfig() {
